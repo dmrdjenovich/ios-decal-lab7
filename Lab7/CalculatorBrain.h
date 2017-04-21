d166d0024850446fd674fa1c161a1f7a70803f57
David Mrdjenovich <dmrdjenovich@users.noreply.github.com>
2017-04-20 19:11:47 -0700

    update

--- Lab7/CalculatorBrain.h
+++ Lab7/CalculatorBrain.h
@@ -26,6 +26,7 @@ typedef enum actionType : long {
 - (void)negateCalculationString;
 - (BOOL)hasDecimalPlaces: (double)operand1 _:(double)operand2;
 - (void)tangentCalculationString;
+- (void)naturalLogCalculationString;
 - (void)commitAction;
 - (BOOL)recognizeAction: (int)tag;
 @end
